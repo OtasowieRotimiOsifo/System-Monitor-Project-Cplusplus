df2411c082fa80c7e4a3f9bab2d0d8ccb08389ea
Otasowie <Otasowie.Osifo@physics.org>
2025-02-10 16:10:28 +0100

    Created a new repository for this project in github.

---
+++ build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/format.cpp" "CMakeFiles/monitor.dir/src/format.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/format.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/linux_parser.cpp" "CMakeFiles/monitor.dir/src/linux_parser.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/linux_parser.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/main.cpp" "CMakeFiles/monitor.dir/src/main.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/main.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/ncurses_display.cpp" "CMakeFiles/monitor.dir/src/ncurses_display.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/ncurses_display.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/parsers.cpp" "CMakeFiles/monitor.dir/src/parsers.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/parsers.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/precess_data_parsers.cpp" "CMakeFiles/monitor.dir/src/precess_data_parsers.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/precess_data_parsers.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/process.cpp" "CMakeFiles/monitor.dir/src/process.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/process.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/processor.cpp" "CMakeFiles/monitor.dir/src/processor.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/processor.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/system.cpp" "CMakeFiles/monitor.dir/src/system.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/system.cpp.o.d"
+  "/media/otasowie-osifo/Data Store/VM-SHARE/vm-share/code-dev/C++/System-Monitor-Project-Cplusplus/src/system_data_parsers.cpp" "CMakeFiles/monitor.dir/src/system_data_parsers.cpp.o" "gcc" "CMakeFiles/monitor.dir/src/system_data_parsers.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/monitor.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monitor.dir/src/format.cpp.o"
+  "CMakeFiles/monitor.dir/src/format.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "CMakeFiles/monitor.dir/src/linux_parser.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/main.cpp.o"
+  "CMakeFiles/monitor.dir/src/main.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "CMakeFiles/monitor.dir/src/ncurses_display.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/parsers.cpp.o"
+  "CMakeFiles/monitor.dir/src/parsers.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/precess_data_parsers.cpp.o"
+  "CMakeFiles/monitor.dir/src/precess_data_parsers.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/process.cpp.o"
+  "CMakeFiles/monitor.dir/src/process.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "CMakeFiles/monitor.dir/src/processor.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/system.cpp.o"
+  "CMakeFiles/monitor.dir/src/system.cpp.o.d"
+  "CMakeFiles/monitor.dir/src/system_data_parsers.cpp.o"
+  "CMakeFiles/monitor.dir/src/system_data_parsers.cpp.o.d"
+  "monitor"
+  "monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
